1317160dfc3a8d36e59c370c5c57046cb4b19c93
neil davidson <neil.j.davidson@gmail.com>
2020-05-05 15:12:47 +0100

    initial commit

---
+++ projects/top-twenty/precache-manifest.2b02a02ee9f1c478d487f5a64db502fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e9dc01aeb411b7fb2f4a7ecfb6ea9f",
+    "url": "/projects/top-twenty/index.html"
+  },
+  {
+    "revision": "794671c8ac96df25d088",
+    "url": "/projects/top-twenty/static/css/main.a8e5da09.chunk.css"
+  },
+  {
+    "revision": "04c9709e61a4603efb56",
+    "url": "/projects/top-twenty/static/js/2.c75339ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/top-twenty/static/js/2.c75339ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794671c8ac96df25d088",
+    "url": "/projects/top-twenty/static/js/main.69c9ced4.chunk.js"
+  },
+  {
+    "revision": "3fff7335e7380a42815a",
+    "url": "/projects/top-twenty/static/js/runtime-main.88029eef.js"
+  }
+]);
