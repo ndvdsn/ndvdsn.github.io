cd94418b48161f7a20f97fb384439f88011a4ce4
neil davidson <neil.j.davidson@gmail.com>
2020-05-05 19:18:12 +0100

    adds tetris project

---
+++ projects/react-tetris/precache-manifest.1d572ec9c6e4a75c544326e0caef8f2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740b96e27a3c1f9cf346b42f9369c026",
+    "url": "/projects/react-tetris/index.html"
+  },
+  {
+    "revision": "79520fff3ef3e53748f8",
+    "url": "/projects/react-tetris/static/css/main.957982a2.chunk.css"
+  },
+  {
+    "revision": "a2d6706b8e10b42f1b39",
+    "url": "/projects/react-tetris/static/js/2.cbb0049e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projects/react-tetris/static/js/2.cbb0049e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79520fff3ef3e53748f8",
+    "url": "/projects/react-tetris/static/js/main.89b10306.chunk.js"
+  },
+  {
+    "revision": "21189aeb85340de7f61c",
+    "url": "/projects/react-tetris/static/js/runtime-main.83bc9f2c.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/projects/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/projects/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
